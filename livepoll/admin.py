461b819e73362b05b7d43c746f1d721ccc44d961
geuresti <54899735+geuresti@users.noreply.github.com>
2022-05-01 16:26:08 -0400

    Delete livepoll directory

--- livepoll/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Poll, Answer
-
-admin.site.register(Poll)
-admin.site.register(Answer)
--- livepoll/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class LivepollConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'livepoll'
